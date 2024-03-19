95df5750fb3e0a18a006e6a191d725177214ea1a
Rkesh Roushan <rakeshroushan2341@gmail.com>
2024-03-20 00:24:19 +0530

    SocioNFT with Reel Section

--- server/helpers/savePhoto.js
+++
@@ -1,8 +0,0 @@
-const savePhoto = (post, photoEncode, photoType) => {
-	if (photoEncode != null) {
-		post.Photo = new Buffer.from(photoEncode, "base64");
-		post.PhotoType = photoType;
-	}
-};
-
-module.exports = { savePhoto };
